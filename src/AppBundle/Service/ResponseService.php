1aa9d5508ebc2867adadbeb597da66e04a132f85
Vincent Rasquier <vrasquier@keley-live.com>
2017-03-13 10:48:38 +0100

    Better response service

--- src/AppBundle/Service/ResponseService.php
+++ src/AppBundle/Service/ResponseService.php
@@ -6,14 +6,15 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ResponseService
 {
-    public function sendError($code, $msg, $callback = null)
+    public function send($status, $code, $data, $error, $callback = null)
     {
         $response = new JsonResponse();
         $response->setStatusCode(200);
         $response->setData(array(
-            'status' => false,
-            'code' => $code,
-            'data' => $msg
+            'status'    => $status,
+            'code'      => $code,
+            'data'      => $data,
+            'error'     => $error
         ));
 
         if ($callback) {
@@ -22,21 +23,14 @@ class ResponseService
 
         return $response;
     }
-
-    public function sendSuccess($body, $callback = null)
+    
+    public function sendError($code, $error, $callback = null)
     {
-        $response = new JsonResponse();
-        $response->setStatusCode(200);
-        $response->setData(array(
-            'status' => true,
-            'code' => 0,
-            'data' => $body
-        ));
+        return $this->send(false, $code, null, $error, $callback);
+    }
 
-        if ($callback) {
-            $response->setCallback($callback);
-        }
-
-        return $response;
+    public function sendSuccess($data, $callback = null)
+    {
+        return $this->send(true, 0, $data, null, $callback);
     }
 }
