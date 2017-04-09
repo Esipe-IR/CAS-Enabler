b3a7a36ebf1641a9b5f0541ed20c64cdd993f774
Vincent Rasquier <vrasquier@keley-live.com>
2017-04-09 21:53:54 +0200

    Add Ical parser + edt action start

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use ICal\ICal;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,6 +33,26 @@ class MainController extends Controller
      */
     public function edtAction(Request $request)
     {
-        //https://edt.u-pem.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=1813,1806,1812,1811,1807,1640,5314&projectId=19&calType=ical&nbWeeks=4&sqlMode=true
+        //1813,1806,1812,1811,1807,1640,5314
+        $r = "resources=" . $request->query->get("resources");
+        $p = "projectId=19";
+        $t = "calType=ical";
+        $n = "nbWeeks=6";
+        $s = "sqlMode=true";
+        $url = "https://edt.u-pem.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?$r&$p&$t&$n&$s";
+
+        $r = file_get_contents($url);
+        $arr = explode("\n", $r);
+        $i = new ICal($arr);
+
+        $events = $i->eventsFromRange('first day of this month');
+        $resp = array();
+
+        foreach ($events as $event) {
+            $dtstart = new \DateTime('@' . (int) $i->iCalDateToUnixTimestamp($event->dtstart));
+            $resp[$dtstart->format("d_m")][] = $event;
+        }
+
+        dump($resp);die;
     }
 }
